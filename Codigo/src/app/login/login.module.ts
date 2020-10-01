346c8da5a6c5e72947557fe9312d320ee646411f
NicolasSoler <josolerh@poligran.edu.co>
2020-10-01 10:41:34 -0500

    correccion de errores

--- Codigo/src/app/login/login.module.ts
+++ Codigo/src/app/login/login.module.ts
@@ -1,24 +1,9 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const bodyParser = require('body-parser');
-const { url } = require('./config/database');
-mongoose.connect(url, {
-    useMongoClient: true
-});
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
 import { NgModule, NO_ERRORS_SCHEMA, OnInit } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { loginRoutingModule } from "./login-routing-module";
 import { loginComponent } from "./login.component";
-import { userSchema } from "../../models/user";
+
 
 
 @NgModule({
@@ -35,19 +20,10 @@ import { userSchema } from "../../models/user";
 })
 export class loginModule implements OnInit{ 
 
-    user= userSchema;
-
     constructor(){
-        this.user = new userSchema;
-
     }
 
     ngOnInit(): void {
         
     }
-
-    onNavigate(){
-        console.log(this.user);
-    }
-
 };
