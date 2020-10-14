f4ce0dfb8add9d665558e6727505bef45e4f7d25
NicolasSoler <josolerh@poligran.edu.co>
2020-10-14 00:32:48 -0500

    Correccion de daños

--- Codigo/src/app/registro/registro.module.ts
+++ Codigo/src/app/registro/registro.module.ts
@@ -1,11 +1,9 @@
 import { NgModule, NO_ERRORS_SCHEMA, OnInit } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
-import { from } from "rxjs";
 
 import { registroRoutingModule } from "./registro-routing-module";
 import { registroComponent } from "./registro.component";
-import { UserService } from "../services/user_service/user.services";
-import { User } from "../models/user.model";
+
 
 
 @NgModule({
@@ -21,18 +19,4 @@ import { User } from "../models/user.model";
     ]
 })
 
-export class registroModule implements OnInit{ 
-    newUser: User;
-    
-    constructor (private userService: UserService){
-        this.newUser = new User();
-    }
-    createUser () {
-        console.log("-->", this.newUser);
-        this.userService.createUser(this.newUser);
-    }
-
-    ngOnInit(): void {
-
-    }
-}
+export class registroModule{ }
