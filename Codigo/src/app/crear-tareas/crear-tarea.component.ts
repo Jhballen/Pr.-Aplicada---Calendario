90ecfe79dbcc05401ce824bc93a8fa9fb56ef189
NicolasSoler <josolerh@poligran.edu.co>
2020-10-22 11:21:40 -0500

    Correccion de nombres

--- Codigo/src/app/crear-tareas/crear-tarea.component.ts
+++ Codigo/src/app/crear-tareas/crear-tarea.component.ts
@@ -37,7 +37,7 @@ export class crearTareaComponent implements OnInit{
 
     createTask(){
         this.newTarea.status = 0;
-        this.newTarea.profiles = enviroment.profile;
+        this.newTarea.profile = enviroment.profile;
         console.log(this.newTarea)
         this.taskService.createTask(this.newTarea);
         this.onNavigate();
